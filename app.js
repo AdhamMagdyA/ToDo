8d8c6e07de4d37f63b53aa78e445647b58d9ced9
AdhamMagdyA <adhammagdy.dev@gmail.com>
2023-03-24 19:27:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+const todoInput = document.getElementById("todo-input");
+const addTodoButton = document.getElementById("add-todo");
+const todoList = document.getElementById("todo-list");
+let todos = [];
+
+function renderTodoList() {
+  todoList.innerHTML = "";
+  todos.forEach((todo, index) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <span>${todo}</span>
+      <button class="delete-todo" data-index="${index}">Delete</button>
+    `;
+    todoList.appendChild(li);
+  });
+}
+
+function addTodo() {
+  const todo = todoInput.value.trim();
+  if (!todo) {
+    return;
+  }
+  todos.push(todo);
+  renderTodoList();
+  todoInput.value = "";
+}
+
+function deleteTodoByIndex(index) {
+  if (index < 0 || index >= todos.length) {
+    return;
+  }
+  todos.splice(index, 1);
+  renderTodoList();
+}
+
+addTodoButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  addTodo();
+});
+
+todoList.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-todo")) {
+    const index = parseInt(event.target.dataset.index);
+    deleteTodoByIndex(index);
+  }
+});
